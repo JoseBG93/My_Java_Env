6ed2f84b89ef08643cc5362cfbff66d3ec9c5d5b
Josh <jlburguera93@gmail.com>
2025-06-18 13:19:45 +0200

    Add src directory

---
+++ src/main/java/com/holamundocourse/classes/numberFormat.java
@@ -0,0 +1,17 @@
+package com.holamundocourse.classes;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class numberFormat {
+    public static void main(String[] args) {
+        Locale locale = Locale.forLanguageTag("es-ES");
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale); // This NumberFormat's method allows us to format numbers as currency according to the specified locale.
+        String result = numberFormat.format(3000); 
+        System.out.println("Formatted number: " + result);
+
+        NumberFormat percentage = NumberFormat.getPercentInstance(); 
+        System.out.println(percentage.format(0.35));// This NumberFormat's method allows us to format numbers as percentage.
+    }
+}
+
---
+++ src/main/java/com/holamundocourse/classes/strings.java
@@ -0,0 +1,35 @@
+package com.holamundocourse.classes;
+// String is the most important reference data type. By using it, we can create text within our apps.
+
+public class strings {
+    public static void main(String[] args) {
+        String text = new String("Hi, World!");
+        System.out.println(text);
+// Besides what we see before, we can type the following code:
+// [...] String text = "Hi, World";
+// In this case, we remove 'new String' and the brackets ().
+// This way is called "literal strings"
+
+        // Remember: first, you specify the data type (in this case, 'int'); then, the name of the variable.
+        // As you can see, after '=' there's the class variable name ('text') and a method called '.length()', which is the large of the string "Hi, world!".
+        int large = text.length();
+        System.out.println(large);
+
+        String newText = text.replace("Hi", "Bye"); // Replace "Hi" by "Bye".
+        // The content inside the brackets is called 'argument'. So, text.replace has two arguments: "Hi" and "Bye".
+        // Also, strings are changeless; therefore, we cannot modify its content applying a method.
+        System.out.println(newText);
+
+        System.out.println(text.endsWith("rld!")); // Check the ending of a variable text.
+        System.out.println(text.startsWith("Hi")); // Check the beginning of a variable text.
+        System.out.println(text.contains("Wo")); // Check if a variable text contains any character.
+        System.out.println(text.indexOf("Wor")); // Check the index of a character (the position within a string).
+
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+
+        String wrong = "  happy     birthday    ";
+        System.out.println(wrong.trim()); // This method erases the spaces from the beginning and the end of the text.
+        System.out.println(wrong);
+    }
+}
